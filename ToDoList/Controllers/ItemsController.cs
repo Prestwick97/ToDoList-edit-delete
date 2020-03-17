f1e46eea02cd907f055b6a39d111ac238e61f22b
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-16 18:14:25 -0700

    Updated Models and Controllers to user EF Core

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,50 +1,74 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using ToDoList.Models;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
+    private readonly ToDoListContext _db;
 
-    [HttpGet("/items")]
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
-    
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      myItem.Save();
-      return RedirectToAction("Index");
-    }
-
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
-
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      List<Item> model = _db.Items.ToList();
       return View(model);
     }
   }
 }
+
+// using Microsoft.AspNetCore.Mvc;
+// using System.Collections.Generic;
+// using ToDoList.Models;
+// using System;
+
+// namespace ToDoList.Controllers
+// {
+//   public class ItemsController : Controller
+//   {
+
+//     [HttpGet("/items")]
+//     public ActionResult Index()
+//     {
+//       List<Item> allItems = Item.GetAll();
+//       return View(allItems);
+//     }
+
+//     [HttpGet("/items/new")]
+//     public ActionResult New()
+//     {
+//       return View();
+//     }
+    
+//     [HttpPost("/items")]
+//     public ActionResult Create(string description)
+//     {
+//       Item myItem = new Item(description);
+//       myItem.Save();
+//       return RedirectToAction("Index");
+//     }
+
+//     [HttpPost("/items/delete")]
+//     public ActionResult DeleteAll()
+//     {
+//       Item.ClearAll();
+//       return View();
+//     }
+
+//     [HttpGet("/categories/{categoryId}/items/{itemId}")]
+//     public ActionResult Show(int categoryId, int itemId)
+//     {
+//       Item item = Item.Find(itemId);
+//       Category category = Category.Find(categoryId);
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       model.Add("item", item);
+//       model.Add("category", category);
+//       return View(model);
+//     }
+//   }
+// }
